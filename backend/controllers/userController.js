fe8a359d792e5c75f2a7289631b6a09dd4062a8e
bharat-yosa <bharat.joshi@appsontechnologies.in>
2024-03-20 12:58:53 +0530

    Add login and logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
-import {User} from "../models/userSchema.js";
+import { User } from "../models/userSchema.js";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 export const Register = async (req, res) => {
   try {
-    console.log("user controller")
+    console.log("user controller");
     const { name, username, email, password } = req.body;
     if (!name || !username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
@@ -16,14 +17,13 @@ export const Register = async (req, res) => {
 
     const hashedPassword = await bcryptjs.hash(password, 12);
 
-
-    console.log(name, username, email, hashedPassword)
+    console.log(name, username, email, hashedPassword);
 
     const newUser = await User.create({
       name,
       username,
       email,
-      hashedPassword,
+      password: hashedPassword,
     });
 
     return res
@@ -33,3 +33,38 @@ export const Register = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const tokendata = { userid: user._id };
+    const token = await jwt.sign({ tokendata }, process.env.TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, { expiresIn: "1h", httpOnly: true })
+      .json({ token: token, message: "Login successful" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const Logout =(req,res)=>{
+  return res.cookie("token","",{expiresIn: new Date(Date.now)}).json({message:"Logged out successfully"})
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,9 @@
 import  express  from "express";
 const router = express.Router();
-import { Register } from "../controllers/userController.js";
+import { Register,Login,Logout } from "../controllers/userController.js";
 
 router.route("/register").post(Register);
+router.route("/login").post(Login)
+router.route("/logout").get(Logout)
 
 export default router;
